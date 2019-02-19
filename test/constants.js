152bbe22cec0ece9ce6244c7438d8caa0c695707
Deric Walintukan <deric.walintukan@gmail.com>
2019-02-19 11:58:43 +0700

    Copy tests

---
+++ test/constants.js
@@ -0,0 +1,7 @@
+module.exports = (accounts) => Object.freeze({
+  OWNER: accounts[0],
+  ACCT1: accounts[1],
+  ACCT2: accounts[2],
+  ACCT3: accounts[3],
+  INVALID_ADDR: '0x0000000000000000000000000000000000000000',
+})
---
+++ test/token/eusd.tests.js
@@ -0,0 +1,126 @@
+const { assert } = require('chai')
+
+const TimeMachine = require('../util/time-machine')
+const sassert = require('../util/sol-assert')
+const getConstants = require('../constants')
+const GRC223PreMinted = require('../data/grc223-pre-minted')
+
+const web3 = global.web3
+
+contract('EUSD', (accounts) => {
+  const { OWNER, INVALID_ADDR } = getConstants(accounts)
+  const TOKEN_PARAMS = {
+    name: 'TestToken',
+    symbol: 'TTT',
+    decimals: 8,
+    totalSupply: 10000000000,
+    owner: OWNER,
+  }
+  const timeMachine = new TimeMachine(web3)
+
+  let token
+
+  beforeEach(async () => {
+    await timeMachine.snapshot
+
+    token = new web3.eth.Contract(GRC223PreMinted.abi)
+    token = await token.deploy({
+      data: GRC223PreMinted.bytecode,
+      arguments: [
+        TOKEN_PARAMS.name,
+        TOKEN_PARAMS.symbol, 
+        TOKEN_PARAMS.decimals,
+        TOKEN_PARAMS.totalSupply,
+        TOKEN_PARAMS.owner,
+      ],
+    }).send({ from: OWNER, gas: 4712388 })
+  })
+  
+  afterEach(async () => {
+    await timeMachine.revert
+  })
+
+  describe('constructor', async () => {
+    it('should initialize all the values correctly', async () => {
+      assert.equal(await token.methods.owner().call(), TOKEN_PARAMS.owner)
+      assert.equal(await token.methods.name().call(), TOKEN_PARAMS.name)
+      assert.equal(await token.methods.symbol().call(), TOKEN_PARAMS.symbol)
+      assert.equal(await token.methods.decimals().call(), TOKEN_PARAMS.decimals)
+      assert.equal(await token.methods.totalSupply().call(), TOKEN_PARAMS.totalSupply)
+      assert.equal(await token.methods.balanceOf(TOKEN_PARAMS.owner).call(), TOKEN_PARAMS.totalSupply)
+    })
+
+    it('should emit both Transfer events', async () => {
+      sassert.event(token, 'Transfer')
+      sassert.event(token, 'Transfer223')
+    })
+
+    it('throws if owner is not valid', async () => {
+      try {
+        await token.deploy({
+          data: GRC223PreMinted.bytecode,
+          arguments: [
+            TOKEN_PARAMS.name,
+            TOKEN_PARAMS.symbol, 
+            TOKEN_PARAMS.decimals,
+            TOKEN_PARAMS.totalSupply,
+            INVALID_ADDR,
+          ],
+        }).send({ from: OWNER, gas: 4712388 })
+      } catch (e) {
+        sassert.revert(e)
+      }
+    })
+
+    it('throws if name is empty', async () => {
+      try {
+        await token.deploy({
+          data: GRC223PreMinted.bytecode,
+          arguments: [
+            '',
+            TOKEN_PARAMS.symbol, 
+            TOKEN_PARAMS.decimals,
+            TOKEN_PARAMS.totalSupply,
+            TOKEN_PARAMS.owner,
+          ],
+        }).send({ from: OWNER, gas: 4712388 })
+      } catch (e) {
+        sassert.revert(e)
+      }
+    })
+
+    it('throws if symbol is empty', async () => {
+      try {
+        await token.deploy({
+          data: GRC223PreMinted.bytecode,
+          arguments: [
+            TOKEN_PARAMS.name, 
+            '',
+            TOKEN_PARAMS.decimals,
+            TOKEN_PARAMS.totalSupply,
+            TOKEN_PARAMS.owner,
+          ],
+        }).send({ from: OWNER, gas: 4712388 })
+      } catch (e) {
+        sassert.revert(e)
+      }
+    })
+
+    it('throws if totalSupply is not greater than 0', async () => {
+      try {
+        await token.deploy({
+          data: GRC223PreMinted.bytecode,
+          arguments: [
+            TOKEN_PARAMS.name, 
+            TOKEN_PARAMS.symbol,
+            TOKEN_PARAMS.decimals,
+            0,
+            TOKEN_PARAMS.owner,
+          ],
+        }).send({ from: OWNER, gas: 4712388 })
+      } catch (e) {
+        sassert.revert(e)
+      }
+    })
+  })
+})
